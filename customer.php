81b8f13c3c8c4d8bdb6927f140b029f373e90f5b
Tom <azwhole@hotmail.com>
2015-03-23 14:34:59 -0700

    something

--- customer.php
+++ customer.php
@@ -1,17 +1,49 @@
 <h1><center> CUSTOMER </center></h1>
 
-<?php
+<h2>Delivery Status</h2>
+<form> 
+	Issue Number: <input type="text" name="issuenumber">
+	<input type="submit" name="GetInfo">
+</form>
 
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "project";
 
-?>
-<h2>Delivery Status</h2>
-<form> 
-	Issue Number: <input type="text" name="issuenumber">
-	<input type="submit">
-</form>
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "select Mailed.issuenumber, 
+		HasShipmentMethod.issuenumber,
+		Mailed.deliverystatus, 
+		Mailed.receiverstatus, 
+		HasShipmentMethod.duration
+		FROM Mailed
+		INNER JOIN HasShipmentMethod
+		ON Mailed.issuenumber = HasShipmentMethod.issuenumber";
+$result = mysqli_query($conn, $sql);
 
+
+Echo "<Table>";
+Echo "<TR><TD>Issue Number</TD>
+		  <TD>Delivery Status</TD>
+		  <TD>Reciever Status</TD>
+		  <TD>Duration</TD> </TR>";
+if (mysqli_num_rows($result) > 0) {
+	while($array = mysqli_fetch_array($result)) {
+		echo "<TR><TD> $array[0] </TD>";
+		echo 	 "<TD> $array[1] </TD>";
+		echo 	 "<TD> $array[2] </TD>";
+		echo 	 "<TD> $array[3] </TD></TR>";
+	}
+}
+
+mysqli_close($conn);
+
+?>
 <h2> Change Delivery Method </h2>
