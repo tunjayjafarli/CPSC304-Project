34a20698f0999ea4bbcf3689a42856e1505acdce
Tom <azwhole@hotmail.com>
2015-03-23 09:48:28 -0700

    nothing much

---
+++ customer.php
@@ -0,0 +1,16 @@
+<h1><center> CUSTOMER </center></h1>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project";
+
+?>
+<h2>Delivery Status</h2>
+<form> Issue Number: <input type="text" name="issuenumber">
+
+
+</form>
+<h2> Change Delivery Method </h2>
