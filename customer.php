ae1927e93faaeacb8ba32e6db8644ba7870ce423
Tom <azwhole@hotmail.com>
2015-03-23 21:51:08 -0700

    what I have done so far

--- customer.php
+++ customer.php
@@ -1,16 +1,33 @@
 <h1><center> CUSTOMER </center></h1>
 
+<TABLE border=0 height=10% width=100% bgcolor="lightgrey">
+		<TR>
+			
+		<TD><a href="header.php"> HOME </a></TD>
+		<TD><a href="branch.php"> BRANCHES </a></TD>
+		<TD><a href="customer.php"> CUSTOMER </a></TD>
+		<TD><a href="employee.php"> EMPLOYEE </a></TD>
+
+		</TR> 
+</TABLE>
 <h2>Delivery Status</h2>
-<form> 
+<form method="POST" action="customer.php"> 
 	Issue Number: <input type="text" name="issuenumber">
 	<input type="submit" name="GetInfo">
+</form>
+<h2> Change Delivery Method </h2>
+<form method="POST" action="customer.php"> 
+	Issue Number: <input type="text" name="issuenumber">
+	<input type="radio" name="method" value="regular">Regular
+   	<input type="radio" name="method" value="express">Express
+	<input type="submit" name="ChangeDeliveryMethod">
 </form>
 
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "project";
+$dbname = "PostalService";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -18,32 +35,59 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "select Mailed.issuenumber, 
-		HasShipmentMethod.issuenumber,
-		Mailed.deliverystatus, 
-		Mailed.receiverstatus, 
-		HasShipmentMethod.duration
-		FROM Mailed
-		INNER JOIN HasShipmentMethod
-		ON Mailed.issuenumber = HasShipmentMethod.issuenumber";
-$result = mysqli_query($conn, $sql);
 
+$number = 0;
 
-Echo "<Table>";
-Echo "<TR><TD>Issue Number</TD>
-		  <TD>Delivery Status</TD>
-		  <TD>Reciever Status</TD>
-		  <TD>Duration</TD> </TR>";
-if (mysqli_num_rows($result) > 0) {
-	while($array = mysqli_fetch_array($result)) {
-		echo "<TR><TD> $array[0] </TD>";
-		echo 	 "<TD> $array[1] </TD>";
-		echo 	 "<TD> $array[2] </TD>";
-		echo 	 "<TD> $array[3] </TD></TR>";
+if(array_key_exists('GetInfo', $_POST)){
+
+	if(is_int($number)){
+
+		$number = htmlspecialchars($_POST["issuenumber"]);
+	
+		$sql = "select Mailed.issuenumber, 
+				Mailed.deliverystatus, 
+				Mailed.receiverstatus, 
+				HasShipmentMethod.duration
+				from Mailed
+				inner join HasShipmentMethod
+				on Mailed.issuenumber = HasShipmentMethod.issuenumber
+				where Mailed.issuenumber = $number";
+		$result = mysqli_query($conn, $sql);
+		if(is_object($result)){
+
+			Echo "<Table border = 1>";
+			Echo "<TR><TD>Issue Number</TD>
+				  <TD>Delivery Status</TD>
+				  <TD>Reciever Status</TD>
+				  <TD>Duration</TD> </TR>";
+			if (mysqli_num_rows($result) > 0) {
+				while($array = mysqli_fetch_array($result)) {
+					echo "<TR><TD> $array[0] </TD>";
+					echo 	 "<TD> $array[1] </TD>";
+					echo 	 "<TD> $array[2] </TD>";
+					echo 	 "<TD> $array[3] </TD></TR>";
+				}
+			}
+			Echo "</Table>";
+		}
+		else{
+		echo "Please enter an issue number";
+		}
 	}
+	
+}
+
+//not too sure yet.
+if(array_key_exists('ChangeDeliveryMethod', $_POST)){
+	$number = htmlspecialchars($_POST["issuenumber"]);
+	$sql = "select Mailed.deliverystatus, 
+				Mailed.receiverstatus, 
+				from Mailed
+				where Mailed.issuenumber = $number";
+	$result = mysqli_query($conn, $sql);
 }
 
 mysqli_close($conn);
 
 ?>
-<h2> Change Delivery Method </h2>
+
--- employee.php
+++ employee.php
@@ -1,15 +1,144 @@
 
+
 <HTML>
 	<BODY>
-		<h1> EMPLOYEE</h1>
-		<?php
-		Include ("header.php");
+		<h1><center>EMPLOYEE</center></h1>
+<TABLE border=0 height=10% width=100% bgcolor="lightgrey">
+		<TR>
+			
+		<TD><a href="header.php"> HOME </a></TD>
+		<TD><a href="branch.php"> BRANCHES </a></TD>
+		<TD><a href="customer.php"> CUSTOMER </a></TD>
+		<TD><a href="employee.php"> EMPLOYEE </a></TD>
+
+		</TR> 
+</TABLE>
+
+<h2>Shipment Status</h2>
+<form method="POST" action="employee.php"> 
+	Issue Number: <input type="text" name="issuenumber">
+	<input type="submit" name="GetInfo">
+</form>
+<h2>Check Shipment Information</h2>
+<h2>Storage Office Management</h2>
+<?php
+Include ("header.php");
 		Echo "This page is for EMPLOYEES ONLY!"
 		?>
 		<br> </br>
 		<a href="branch.php"> BRANCHES </a> 
 		<br> </br>
 		<a href="packages.php"> IN STORAGE PACKAGES </a>
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "PostalService";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_GET['Submit'])) {
+	$branchid = $_GET['branchid'];
+	$phone = $_GET['phone'];
+	$name = $_GET['name'];
+	$location = $_GET['location'];
+
+	mysqli_query($conn, "insert into Branch value 
+		('$branchid', '$phone', '$name', '$location')");
+	echo "Values inserted successfully";
+}
+
+$sql = "Select * from Branch";
+$result = mysqli_query($conn, $sql);
+
+echo "<h2>Send New Item</h2>";
+
+Echo "<b>Item</b>";
+Echo "<Table border=1>";
+Echo "<TR><TD>Item Number</TD>
+		  <TD>Fee</TD>
+		  <TD>Size</TD>
+		  <TD>Type</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'itemnumber'></TD>
+		  <TD><Input type = text name = 'fee'></TD>
+		  <TD><Input type = text name = 'size'></TD>
+		  <TD><Input type = text name = 'type'></TD></TR>";
+Echo "</Table>";
+
+Echo "<b>Shipment</b>";
+Echo "<Table border=1>";
+Echo "<TR>
+		  <TD>Issue Number</TD>
+		  <TD>Tracking Number</TD>
+		  <TD>Destination</TD>
+		  <TD>Branch ID</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'issuenumber'></TD>
+		  <TD><Input type = text name = 'trackingnumber'></TD>
+		  <TD><Input type = text name = 'destination'></TD>
+		  <TD><Input type = text name = 'branchid'></TD></TR>";
+Echo "</Table>";
+
+Echo "<b>Shipment Method</b>";
+Echo "<Table border=1>";
+Echo "<TR>
+		  <TD>Duration</TD>
+		  <TD>Method</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'duration'></TD>
+		  <TD><Input type = text name = 'method'></TD></TR>";
+Echo "</Table>";
+
+Echo "<b>Mailing Status</b>";
+Echo "<Table border=1>";
+Echo "<TR>
+		  <TD>Delivery Status</TD>
+		  <TD>Reciever Status</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'deliverystatus'></TD>
+		  <TD><Input type = text name = 'recieverstatus'></TD></TR>";
+Echo "</Table>";
+
+//date arrived or sent
+Echo "<b>Date</b>";
+Echo "<Table border=1>";
+Echo "<TR>
+		  <TD>Day</TD>
+		  <TD>Month</TD>
+		  <TD>Year</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'day'></TD>
+		  <TD><Input type = text name = 'month'></TD>
+		  <TD><Input type = text name = 'year'></TD></TR>";
+Echo "</Table>";
+
+Echo "<b>Payment</b>";
+Echo "<Table border=1>";
+Echo "<TR>
+		  <TD>Receipt Number</TD> 
+		  <TD>Amount</TD>
+		  <TD>Holder Name</TD>
+		  <TD>Card Number</TD>
+		  <TD>Paid Type</TD>
+		  <TD>Shipment Type</TD></TR>";
+Echo "<TR>
+		  <TD><Input type = text name = 'receiptnumber'></TD>
+		  <TD><Input type = text name = 'amount'></TD>
+		  <TD><Input type = text name = 'holdername'></TD>
+		  <TD><Input type = text name = 'cardnumber'></TD>
+		  <TD><Input type = text name = 'ptype'></TD>
+		  <TD><Input type = text name = 'stype'></TD>
+		  <TD><Input type = submit name = 'Submit'></TD></TR>";
+Echo "</Table>";
+
+mysqli_close($conn);
+
+?>
 	</BODY>
 </HTML>
-
