38c9a4d99fce8056689a8236354923e104f08e07
tunjayjafarli <tuncay_ceferli@live.com>
2015-03-23 22:11:51 -0700

    Changed the header

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 		<TABLE border=0 height=10% width=100% bgcolor="blue">
 			<TR>
 
-				<TD><a href="header.php"> HOME </a></TD>
-				<TD><a href="branch.php"> BRANCHES </a></TD>
+				<TD><a href="header.php"> CUSTOMER </a></TD>
+				<TD><a href="employee.php"> EMPLOYEE </a></TD>
 
 			</TR> 
 		</TABLE>
