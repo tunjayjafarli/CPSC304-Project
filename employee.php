06714fd67dbb26d08e5d96253667d5392095e667
tunjayjafarli <tuncay_ceferli@live.com>
2015-03-25 19:13:14 -0700

    changed the ordering of links

--- employee.php
+++ employee.php
@@ -4,10 +4,9 @@ include ("header.php");
 
 // Echo "This page is for EMPLOYEES ONLY!";
 Echo "<br/>
-<a href=branch.php> BRANCHES<br/> </a>
-<a href=logout.php class='btn btn-success'> LOG OUT</a> 
-<br/>
-<a href=packages.php> IN STORAGE PACKAGES </a>";
+<a href=branch.php> BRANCHES </a> <br> <br/>
+<a href=packages.php> IN STORAGE PACKAGES </a> <br> <br/> 
+<a href=logout.php class='btn btn-success'> LOG OUT</a> ";
 
 ?>
 
