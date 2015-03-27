75285b83446456ed3f0dfc7eab4d2a7f70f6fe4a
Asif Mammadov <asif.mammadovv@gmail.com>
2015-03-26 20:05:45 -0700

    Made small changes to UI stuff

--- customer.php
+++ customer.php
@@ -3,21 +3,13 @@
 include ("header.php");
 ?>
 
-<h1><center> CUSTOMER </center></h1>
+<!-- <h1><center> CUSTOMER </center></h1>
+-->
 
 <h2>Delivery Status</h2>
 <form method="POST" action="customer.php"> 
 	Issue Number: <input type="text" name="issuenumber">
 	<input type="submit" name="GetInfo">
-</form>
-<h2> Change Delivery Method </h2>
-<form method="POST" action="customer.php"> 
-	Issue Number: <input type="text" name="issuenumber"><br>
-	Card Number: <input type="text" name="cardnumber"><br>
-	Holder Name: <input type="text" name="holdername"><br>
-	<input type="radio" name="method" value="regular" checked>Regular
-   	<input type="radio" name="method" value="express">Express
-	<input type="submit" name="ChangeDeliveryMethod">
 </form>
 
 <?php
@@ -31,7 +23,7 @@ $dbname = "PostalService";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
 }
 
 $number = 0;
@@ -41,25 +33,26 @@ if(array_key_exists('GetInfo', $_POST)){
 	$number = htmlspecialchars($_POST["issuenumber"]);
 
 	if(is_numeric($number)){
-	
+
 		$sql = "select Mailed.issuenumber, 
-				Mailed.deliverystatus, 
-				Mailed.receiverstatus, 
-				HasShipmentMethod.duration
-				from Mailed
-				inner join HasShipmentMethod
-				on Mailed.issuenumber = HasShipmentMethod.issuenumber
-				where Mailed.issuenumber = $number";
+		Mailed.deliverystatus, 
+		Mailed.receiverstatus, 
+		HasShipmentMethod.duration
+		from Mailed
+		inner join HasShipmentMethod
+		on Mailed.issuenumber = HasShipmentMethod.issuenumber
+		where Mailed.issuenumber = $number";
 		$result = mysqli_query($conn, $sql);
 		if(is_object($result)){
 
-			Echo "<Table border = 1>";
-			Echo "<TR><TD>Issue Number</TD>
-				  <TD>Delivery Status</TD>
-				  <TD>Reciever Status</TD>
-				  <TD>Duration</TD> </TR>";
+
 			if (mysqli_num_rows($result) > 0) {
 				while($array = mysqli_fetch_array($result)) {
+					Echo "<Table class=table>";
+					Echo "<TR><TD>Issue Number</TD>
+					<TD>Delivery Status</TD>
+					<TD>Reciever Status</TD>
+					<TD>Duration</TD> </TR>";
 					echo "<TR><TD> $array[0] </TD>";
 					echo 	 "<TD> $array[1] </TD>";
 					echo 	 "<TD> $array[2] </TD>";
@@ -67,14 +60,28 @@ if(array_key_exists('GetInfo', $_POST)){
 				}
 			}
 			Echo "</Table>";
+
 		}
 		else{
-			echo "Please enter an issue number";
+			echo "<p style=color:red>Please enter a valid issue number";
 		}
 	}
 	
 }
 
+?>
+
+<h2> Change Delivery Method </h2>
+<form method="POST" action="customer.php"> 
+	Issue Number: <input type="text" name="issuenumber"><br>
+	Card Number: <input type="text" name="cardnumber"><br>
+	Holder Name: <input type="text" name="holdername"><br>
+	<input type="radio" name="method" value="regular" checked>Regular
+	<input type="radio" name="method" value="express">Express
+	<input type="submit" name="ChangeDeliveryMethod">
+</form>
+
+<?php
 
 if(array_key_exists('ChangeDeliveryMethod', $_POST)){
 	$issuenumber = htmlspecialchars($_POST["issuenumber"]);
@@ -90,52 +97,52 @@ if(array_key_exists('ChangeDeliveryMethod', $_POST)){
 	}
 
 	if(is_numeric($issuenumber) && is_numeric($cardnumber)){
-	
+
 		
 
 		$checkmethod = "select HasShipmentMethod.method
-						from HasShipmentMethod
-						where HasShipmentMethod.issuenumber = $issuenumber";
+		from HasShipmentMethod
+		where HasShipmentMethod.issuenumber = $issuenumber";
 		$resultmethod = mysqli_query($conn, $checkmethod);
 		$array = mysqli_fetch_array($resultmethod);
 		if(strcmp($method, $array[0]) == 0){
 			echo "Method is already $array[0] ";
 		}else{
 			$updateShipmentSql = "update HasShipmentMethod
-							   inner join Mailed
-							   on HasShipmentMethod.issuenumber = Mailed.issuenumber
-							   set HasShipmentMethod.method = '$method'
-							   where HasShipmentMethod.issuenumber = $issuenumber 
-							   and Mailed.deliverystatus = 'Waiting'";
-		
+			inner join Mailed
+			on HasShipmentMethod.issuenumber = Mailed.issuenumber
+			set HasShipmentMethod.method = '$method'
+			where HasShipmentMethod.issuenumber = $issuenumber 
+			and Mailed.deliverystatus = 'Waiting'";
+
 			$updatePaidSql = "update Paid
-						inner join HasShipmentMethod
-						on Paid.issuenumber = HasShipmentMethod.issuenumber
-						set Paid.amount = Paid.amount + $amount,
-						Paid.ptype = 'Credit Card',
-						Paid.holdername = '$holdername',
-						Paid.cardnumber = $cardnumber
-						where Paid.issuenumber = $issuenumber
-						and not (HasShipmentMethod.method = '%$method%')";
+			inner join HasShipmentMethod
+			on Paid.issuenumber = HasShipmentMethod.issuenumber
+			set Paid.amount = Paid.amount + $amount,
+			Paid.ptype = 'Credit Card',
+			Paid.holdername = '$holdername',
+			Paid.cardnumber = $cardnumber
+			where Paid.issuenumber = $issuenumber
+			and not (HasShipmentMethod.method = '%$method%')";
 
 			$resultShipment = mysqli_query($conn, $updateShipmentSql);
 			$resultPaid = mysqli_query($conn, $updatePaidSql);
 			echo "Shipment was changed successfully.";
 
 			$tablesql = "select HasShipmentMethod.issuenumber, 
-				HasShipmentMethod.method, 
-				Paid.amount
-				from HasShipmentMethod
-				inner join Paid
-				on HasShipmentMethod.issuenumber = Paid.issuenumber 
-				where HasShipmentMethod.issuenumber = $issuenumber";
+			HasShipmentMethod.method, 
+			Paid.amount
+			from HasShipmentMethod
+			inner join Paid
+			on HasShipmentMethod.issuenumber = Paid.issuenumber 
+			where HasShipmentMethod.issuenumber = $issuenumber";
 			$resulttable = mysqli_query($conn, $tablesql);
 			if(is_object($resulttable)){
 
-				Echo "<Table border = 1>";
+				Echo "<Table  class=table>";
 				Echo "<TR><TD>Issue Number</TD>
-					  <TD>Method</TD>
-					  <TD>Amount</TD></TR>";
+				<TD>Method</TD>
+				<TD>Amount</TD></TR>";
 				if (mysqli_num_rows($resulttable) > 0) {
 					while($array = mysqli_fetch_array($resulttable)) {
 						echo "<TR><TD> $array[0] </TD>";
@@ -149,7 +156,7 @@ if(array_key_exists('ChangeDeliveryMethod', $_POST)){
 
 	}
 	else{
-		echo "Issue Number and Card Number must be whole numbers."; 
+		echo "<p style=color:red> Please enter valid Issue Number and Card Number."; 
 	}
 }
 
